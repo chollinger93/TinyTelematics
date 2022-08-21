884d781b537fa7aaf42573a2d8f810b1ac670097
Christian Hollinger <christian@chollinger.com>
2022-08-21 19:52:37 -0400

    Working systemd

--- sbin/run_client.sh
+++ sbin/run_client.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PY_BIN="$HOME/.pyenv/versions/3.8.13/bin/python3"
+source $HOME/.bashrc
 
+echo "Starting with Python env $PY_BIN"
+poetry env use $PY_BIN
 
 if [[ $UID -eq 0 ]]; then
 	SUDO_USER=0
 fi
 
+# TODO: this is dumb because it needs passwordless sudo
 echo "Running setup"
-if [[ -n $SUDO_USER ]]; then
-	/bin/bash $DIR/setup_gps.sh
+if [[ -n $SUDO_USER && $UID -ne 0 ]]; then
+    echo "Running as sudo"
+	sudo /bin/bash $DIR/setup_gps.sh
 else
-    sudo /bin/bash $DIR/setup_gps.sh
+    echo "Running sudo"
+    /bin/bash $DIR/setup_gps.sh
 fi
 
 
@@ -19,8 +26,8 @@ if [[ -n $(~/.pyenv/versions/3.8.13/bin/pip3 list | grep tiny-telematics) || -n 
     echo "Building"
     cd "$DIR/.."
     poetry build
+    poetry install
 fi
 
 echo "Running"
-cd "$DIR/.."
-$(poetry env info | grep Path | cut -d: -f2 | xargs)/bin/python3 -m tiny_telematics.main.py --config $DIR/../config/dev.yaml
+poetry run python -m tiny_telematics.main --config $DIR/../config/dev.yaml
